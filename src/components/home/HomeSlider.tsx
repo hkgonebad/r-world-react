c271f3f622a282ea60bdd81f9e3086807caf2e10
hkgonebad <harry007007@gmail.com>
2024-09-13 18:24:35 +0530

    slider layout

--- src/components/home/HomeSlider.tsx
+++ src/components/home/HomeSlider.tsx
@@ -1,5 +1,6 @@
 import { FiPlayCircle } from "react-icons/fi";
 import Slider from "react-slick";
+import colors from "../../config/colors";
 
 const HomeSlider = () => {
   const settings = {
@@ -14,36 +15,55 @@ const HomeSlider = () => {
     <section className="slider">
       <div className="container">
         <Slider {...settings} className="homeSlider">
-          <div className="hsItem">
-            <div className="hsBox">
-              <img src="/img/slider/1.jpg" alt="" />
-              <div className="captions">
-                <span className="tag badge">Jio</span>
-                <h3>Get your Jio SIM in no time</h3>
-                <p>Now order your Jio SIM and activate it yourself anytime, anywhere.</p>
-                <small>Aug 29, 2024 | 97 Mins</small>
+          <div className="hsItem isVideo">
+            <div className="hsBox row">
+              <div className="col-md-4">
+                <div className="captions">
+                  <span className="tag badge" style={{ backgroundColor: colors.jio }}>
+                    Jio
+                  </span>
 
-                <a href="#" className="btn ">
-                  <FiPlayCircle />
+                  <h3>Get your Jio SIM in no time</h3>
+                  {/* <p>Now order your Jio SIM and activate it yourself anytime, anywhere.</p> */}
+                  <small>Aug 29, 2024 | 97 Mins</small>
+
+                  {/* <a href="#" className="btn ">
+                  <FiPlayCircle className="icon" />
                   Watch Now
-                </a>
+                </a> */}
+                </div>
+              </div>
+              <div className="col-md-8">
+                <div className="hsImg">
+                  <FiPlayCircle className="hsIcon" />
+                  <img src="/img/slider/2.jpg" alt="" />
+                </div>
               </div>
             </div>
           </div>
 
           <div className="hsItem">
-            <div className="hsBox">
-              <img src="/img/slider/1.jpg" alt="" />
-              <div className="captions">
-                <span className="tag badge">Jio</span>
-                <h3>Get your Jio SIM in no time</h3>
-                <p>Now order your Jio SIM and activate it yourself anytime, anywhere.</p>
-                <small>Aug 29, 2024 | 97 Mins</small>
+            <div className="hsBox row">
+              <div className="col-md-4">
+                <div className="captions">
+                  <span className="tag badge" style={{ backgroundColor: colors.nmacc }}>
+                    NMACC
+                  </span>
 
-                <a href="#" className="btn ">
-                  <FiPlayCircle />
+                  <h3>Get your Netflix Subscription</h3>
+                  {/* <p>Now order your Jio SIM and activate it yourself anytime, anywhere.</p> */}
+                  <small>Aug 29, 2024 | 97 Mins</small>
+
+                  {/* <a href="#" className="btn ">
+                  <FiPlayCircle className="icon" />
                   Watch Now
-                </a>
+                </a> */}
+                </div>
+              </div>
+              <div className="col-md-8">
+                <div className="hsImg">
+                  <img src="/img/slider/1.jpg" alt="" />
+                </div>
               </div>
             </div>
           </div>
