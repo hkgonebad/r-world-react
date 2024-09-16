012a1ca145d58d41421ad9bc99fa39af724c7189
hkgonebad <harry007007@gmail.com>
2024-09-16 11:12:33 +0530

    ad banner layouts

--- src/components/home/HomeAdBanner.tsx
+++ src/components/home/HomeAdBanner.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import adbanner from "/img/extras/0.png";
 
 const HomeAdBanner = () => {
-  return <div>HomeAdBanner</div>;
+  return (
+    // <section className="adBanner eventBanner my-3">
+    //   <div className="row">
+    //     <div className="col-md-6">
+    //       <div className="ebDetails">
+    //         <h3>
+    //           International
+    //           <span>Yoga Day</span>
+    //         </h3>
+    //         <p>Unite & Celebrate International Yoga Day 2024</p>
+    //         <small>Sept 12, 2024 | 08:00 am to 09:00 am</small>
+    //         <a href="#!" className="btn btn-primary btn-rounded">
+    //           Know More
+    //         </a>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </section>
+    <section className="adBanner my-3">
+      <a href="#!">
+        <img src={adbanner} alt="" className="w-full" />
+      </a>
+    </section>
+  );
 };
 
 export default HomeAdBanner;
--- src/components/home/HomeAdBanner2.tsx
+++ src/components/home/HomeAdBanner2.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import adbanner from "/img/extras/1.png";
 
 const HomeAdBanner2 = () => {
-  return <div>HomeAdBanner2</div>;
+  return (
+    <section className="adBanner my-3">
+      <a href="#!">
+        <img src={adbanner} alt="" className="w-full" />
+      </a>
+    </section>
+  );
 };
 
 export default HomeAdBanner2;
