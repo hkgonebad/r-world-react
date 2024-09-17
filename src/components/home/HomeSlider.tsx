325e3cec22c46ceee2fcca207235ef738cd2de34
hkgonebad <harry007007@gmail.com>
2024-09-17 11:12:46 +0530

    refactor: reduce redudancy

--- src/components/home/HomeSlider.tsx
+++ src/components/home/HomeSlider.tsx
@@ -2,6 +2,27 @@ import { FiPlayCircle } from "react-icons/fi";
 import Slider from "react-slick";
 import colors from "../../config/colors";
 
+const sliderData = [
+  {
+    tag: "Jio",
+    color: colors.jio,
+    title: "Get your Jio SIM in no time",
+    date: "Aug 29, 2024",
+    duration: "97 Mins",
+    imgSrc: "/img/slider/2.jpg",
+    isVideo: true,
+  },
+  {
+    tag: "NMACC",
+    color: colors.nmacc,
+    title: "Get your Jio SIM in no time",
+    date: "Aug 29, 2024",
+    duration: "97 Mins",
+    imgSrc: "/img/slider/1.jpg",
+    isVideo: false,
+  },
+];
+
 const HomeSlider = () => {
   const settings = {
     dots: true,
@@ -15,58 +36,27 @@ const HomeSlider = () => {
     <section className="slider">
       <div className="container">
         <Slider {...settings} className="homeSlider">
-          <div className="hsItem isVideo">
-            <div className="hsBox row">
-              <div className="col-md-4">
-                <div className="captions">
-                  <span className="tag badge" style={{ backgroundColor: colors.jio }}>
-                    Jio
-                  </span>
-
-                  <h3>Get your Jio SIM in no time</h3>
-                  {/* <p>Now order your Jio SIM and activate it yourself anytime, anywhere.</p> */}
-                  <small>Aug 29, 2024 | 97 Mins</small>
-
-                  {/* <a href="#" className="btn ">
-                  <FiPlayCircle className="icon" />
-                  Watch Now
-                </a> */}
+          {sliderData.map((slide, index) => (
+            <div key={index} className={`hsItem ${slide.isVideo ? "isVideo" : ""}`}>
+              <div className="hsBox row">
+                <div className="col-md-4">
+                  <div className="captions">
+                    <span className="tag badge" style={{ backgroundColor: slide.color }}>
+                      {slide.tag}
+                    </span>
+                    <h3>{slide.title}</h3>
+                    <small>{`${slide.date} | ${slide.duration}`}</small>
+                  </div>
                 </div>
-              </div>
-              <div className="col-md-8">
-                <div className="hsImg">
-                  <FiPlayCircle className="hsIcon" />
-                  <img src="/img/slider/2.jpg" alt="" />
+                <div className="col-md-8">
+                  <div className="hsImg">
+                    {slide.isVideo && <FiPlayCircle className="hsIcon" />}
+                    <img src={slide.imgSrc} alt={slide.title} />
+                  </div>
                 </div>
               </div>
             </div>
-          </div>
-
-          <div className="hsItem">
-            <div className="hsBox row">
-              <div className="col-md-4">
-                <div className="captions">
-                  <span className="tag badge" style={{ backgroundColor: colors.nmacc }}>
-                    NMACC
-                  </span>
-
-                  <h3>Get your Netflix Subscription</h3>
-                  {/* <p>Now order your Jio SIM and activate it yourself anytime, anywhere.</p> */}
-                  <small>Aug 29, 2024 | 97 Mins</small>
-
-                  {/* <a href="#" className="btn ">
-                  <FiPlayCircle className="icon" />
-                  Watch Now
-                </a> */}
-                </div>
-              </div>
-              <div className="col-md-8">
-                <div className="hsImg">
-                  <img src="/img/slider/1.jpg" alt="" />
-                </div>
-              </div>
-            </div>
-          </div>
+          ))}
         </Slider>
       </div>
     </section>
