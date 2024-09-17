cc311988cac026832ccc776370df653cb51fcb92
hkgonebad <harry007007@gmail.com>
2024-09-17 10:55:21 +0530

    fix: responsive css

--- src/containers/HeaderNavbar.tsx
+++ src/containers/HeaderNavbar.tsx
@@ -3,6 +3,8 @@ import { FiCalendar, FiChevronDown, FiFilePlus, FiHeadphones, FiMenu, FiSearch }
 import { Link } from "react-router-dom";
 import logo from "/img/logo.svg";
 import userPic from "/img/user.png";
+import { useState } from "react";
+import TagSelectionModal from "../components/home/TagSelectionModal";
 
 const navLinks = [
   { name: "Home", path: "/" },
@@ -29,97 +31,138 @@ const navLinks = [
   },
 ];
 
-const extraDropdowns = [
-  {
-    icon: FiHeadphones,
-    items: [
-      { name: "Action", path: "/support/action-1" },
-      { name: "Another action", path: "/support/action-2" },
-    ],
-  },
-  {
-    icon: FiFilePlus,
-    items: [
-      { name: "Action", path: "/files/action-1" },
-      { name: "Another action", path: "/files/action-2" },
-    ],
-  },
-  // {
-  //   icon: FiUser,
-  //   items: [
-  //     { name: "Change Password", path: "/profile/change-password" },
-  //     { name: "Logout", path: "/login" },
-  //   ],
-  // },
-];
+// const extraDropdowns = [
+//   {
+//     icon: FiHeadphones,
+//     items: [
+//       { name: "Action", path: "/support/action-1" },
+//       { name: "Another action", path: "/support/action-2" },
+//     ],
+//   },
+//   {
+//     icon: FiFilePlus,
+//     items: [
+//       { name: "Action", path: "/files/action-1" },
+//       { name: "Another action", path: "/files/action-2" },
+//     ],
+//   },
+//   {
+//     icon: FiUser,
+//     items: [
+//       { name: "Change Password", path: "/profile/change-password" },
+//       { name: "Logout", path: "/login" },
+//     ],
+//   },
+// ];
 
 const HeaderNavbar = () => {
+  const [showModal, setShowModal] = useState(false);
+  const availableTags = [
+    "Reliance Industries",
+    "ADAS, AV, & Safety",
+    "EV Battery",
+    "Supply Chain",
+    "Manufacturing",
+    "Sustainable & EV Supply Chain",
+    "EV Strategy",
+    "Charging & Infrastructure",
+    "Regulatory & Policy",
+    "Fleet & Trucking",
+    "Consumer Trends",
+    "Workforce",
+    "Financial Results",
+    "Lifestyle",
+    "Partnerships and M&A",
+    "Products",
+    "Mobility & Transport",
+    "China Strategy",
+    "Aerospace & Defense",
+    "Evironment",
+    "Finance",
+  ];
+
+  const handleClose = () => setShowModal(false);
+
   return (
-    <div className="mainHeader">
-      <Navbar expand="lg" className="">
-        <Container>
-          <Navbar.Brand as={Link} to={"/"}>
-            <img src={logo} alt="R-World Logo" />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="mainNav">
-            <FiMenu />
-          </Navbar.Toggle>
-          <Navbar.Offcanvas id="mainNav" placement="end">
-            <Offcanvas.Header closeButton>
-              <Offcanvas.Title>R-World</Offcanvas.Title>
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-              <Nav className="justify-content-start flex-grow-1 me-auto mainNav">
-                {navLinks.map((link, index) =>
-                  link.dropdown ? (
-                    <NavDropdown
-                      title={
-                        <span className="d-flex align-items-center">
-                          {link.name}
-                          <FiChevronDown className="ms-1 dToggle" />
-                        </span>
-                      }
-                      key={index}
-                    >
-                      {link.dropdown.map((item, idx) => (
-                        <NavDropdown.Item
-                          as={Link}
-                          to={item.path}
-                          key={idx}
-                          active={window.location.pathname === item.path}
-                        >
-                          {item.name}
-                        </NavDropdown.Item>
-                      ))}
-                    </NavDropdown>
-                  ) : (
-                    <Nav.Link as={Link} to={link.path} key={index}>
-                      {link.name}
-                    </Nav.Link>
-                  )
-                )}
-              </Nav>
+    <>
+      <div className="mainHeader">
+        <Navbar expand="lg" className="">
+          <Container>
+            <Navbar.Brand as={Link} to={"/"}>
+              <img src={logo} alt="R-World Logo" />
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="mainNav">
+              <FiMenu />
+            </Navbar.Toggle>
+            <Navbar.Offcanvas id="mainNav" placement="end">
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title>R-World</Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                <Nav className="justify-content-start flex-grow-1 me-auto mainNav">
+                  {navLinks.map((link, index) =>
+                    link.dropdown ? (
+                      <NavDropdown
+                        title={
+                          <span className="d-flex align-items-center">
+                            {link.name}
+                            <FiChevronDown className="ms-1 dToggle" />
+                          </span>
+                        }
+                        key={index}
+                      >
+                        {link.dropdown.map((item, idx) => (
+                          <NavDropdown.Item
+                            as={Link}
+                            to={item.path}
+                            key={idx}
+                            active={window.location.pathname === item.path}
+                          >
+                            {item.name}
+                          </NavDropdown.Item>
+                        ))}
+                      </NavDropdown>
+                    ) : (
+                      <Nav.Link as={Link} to={link.path} key={index}>
+                        {link.name}
+                      </Nav.Link>
+                    )
+                  )}
+                </Nav>
 
-              {/* Search */}
-              <Form className="d-flex hSearch">
-                <Form.Control
-                  type="search"
-                  placeholder="Search here..."
-                  size="sm"
-                  className="me-2"
-                  aria-label="Search"
-                  id="search"
-                />
-                <FiSearch className="hsIcon" />
-              </Form>
+                {/* Search */}
+                <Form className="d-flex hSearch">
+                  <Form.Control
+                    type="search"
+                    placeholder="Search here..."
+                    size="sm"
+                    className="me-2"
+                    aria-label="Search"
+                    id="search"
+                  />
+                  <FiSearch className="hsIcon" />
+                </Form>
 
-              {/* Extras */}
-              <div className="hExtras d-flex gap-2">
-                <a href="#!" className="btn">
-                  <FiCalendar />
-                </a>
+                {/* Extras */}
+                <div className="hExtras d-flex gap-2">
+                  <button onClick={() => {}} className="btn">
+                    <FiCalendar />
+                  </button>
 
-                {extraDropdowns.map((dropdown, index) => (
+                  <button onClick={() => {}} className="btn">
+                    <FiHeadphones />
+                  </button>
+
+                  <button
+                    onClick={() => {
+                      setShowModal(true);
+                    }}
+                    className="btn"
+                  >
+                    <FiFilePlus />
+                  </button>
+
+                  {/* {extraDropdowns.map((dropdown, index) => (
                   <Dropdown align={{ lg: "end" }} key={index}>
                     <Dropdown.Toggle>
                       <dropdown.icon />
@@ -133,27 +176,30 @@ const HeaderNavbar = () => {
                       ))}
                     </Dropdown.Menu>
                   </Dropdown>
-                ))}
+                ))} */}
 
-                <Dropdown align={{ lg: "end" }}>
-                  <Dropdown.Toggle>
-                    <img src={userPic} alt="" />
-                  </Dropdown.Toggle>
-                  <Dropdown.Menu>
-                    <Dropdown.Item as={Link} to={"/"}>
-                      Change Password
-                    </Dropdown.Item>
-                    <Dropdown.Item as={Link} to={"/"}>
-                      Logout
-                    </Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              </div>
-            </Offcanvas.Body>
-          </Navbar.Offcanvas>
-        </Container>
-      </Navbar>
-    </div>
+                  <Dropdown align={{ lg: "end" }}>
+                    <Dropdown.Toggle>
+                      <img src={userPic} alt="" />
+                    </Dropdown.Toggle>
+                    <Dropdown.Menu>
+                      <Dropdown.Item as={Link} to={"/"}>
+                        Change Password
+                      </Dropdown.Item>
+                      <Dropdown.Item as={Link} to={"/"}>
+                        Logout
+                      </Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </div>
+              </Offcanvas.Body>
+            </Navbar.Offcanvas>
+          </Container>
+        </Navbar>
+      </div>
+
+      <TagSelectionModal show={showModal} handleClose={handleClose} availableTags={availableTags} />
+    </>
   );
 };
 
